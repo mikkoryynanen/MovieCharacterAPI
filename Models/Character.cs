23011510930c01d325a24a1122fb47c13b63ce13
arttuhar <arttuhar@gmail.com>
2022-02-24 13:18:13 +0200

    FEAT: Add character, movie and franchise models

---
+++ Models/Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MovieCharacterAPI.Models
+
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Alias { get; set; }
+        public string Gender { get; set; }
+        public string CharacterPicture { get; set; }
+        public ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace MovieCharacterAPI.Models
+
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieTitle { get; set; }
+        public string Genre { get; set; }
+        public string ReleaseYear { get; set; }
+        public string Director { get; set; }
+        public string MoviePicture { get; set; }
+        public string Trailer { get; set; }
+        public ICollection<Character> Characters { get; set; }
+        public int FranchiseId { get; set; }
+        public Franchise Franchise { get; set; }
+    }
+}
