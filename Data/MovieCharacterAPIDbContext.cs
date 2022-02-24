16d2c6785882e1e3e37249c2bbf146041a749f83
arttuhar <arttuhar@gmail.com>
2022-02-24 14:17:49 +0200

    FEAT: Add connection string

---
+++ Data/MovieCharacterAPIDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCharacterAPI.Models;
+
+namespace MovieCharacterAPI.Data
+{
+    public class MovieCharacterAPIDbContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Franchise> Franchises { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=MovieCharacterAPIDb;Integrated Security=True;");
+        }
+    }
+}
