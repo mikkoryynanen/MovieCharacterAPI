51eb78368334dd57a0d5440cd67149de5ffc3ce9
Mikko <mikko.ryynanen2@gmail.com>
2022-03-01 18:57:28 +0200

    FEAT: Added MovieController and it's DTOs

---
+++ Models/DTOs/MovieCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MovieCharacterAPI.Models.DTOs
+{
+    public class MovieCreateDto
+    {
+        public string MovieTitle { get; set; }
+        public string Genre { get; set; }
+        public string ReleaseYear { get; set; }
+        public string Director { get; set; }
+        public string MoviePicture { get; set; }
+        public string Trailer { get; set; }
+        public int FranchiseId { get; set; }
+    }
+}
---
+++ Models/DTOs/MovieDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieCharacterAPI.Models.DTOs
+{
+    public class MovieDto
+    {
+        public string MovieTitle { get; set; }
+
+        public string Genre { get; set; }
+
+        public string ReleaseYear { get; set; }
+                
+        public string Director { get; set; }
+        public string MoviePicture { get; set; }
+        public string Trailer { get; set; }
+        //public ICollection<CharacterDto> Characters { get; set; }
+        public int FranchiseId { get; set; }
+        //public Franchise Franchise { get; set; }
+    }
+}
