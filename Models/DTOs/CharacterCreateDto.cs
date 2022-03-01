d01d71d52dbf662c8e48c5944258c9f74350914b
Mikko <mikko.ryynanen2@gmail.com>
2022-03-01 18:57:41 +0200

    FEAT: Added CharacterController and it's DTOs

---
+++ Models/DTOs/CharacterCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieCharacterAPI.Models.DTOs
+{
+    public class CharacterCreateDto
+    {
+        public string FullName { get; set; }
+        public string Alias { get; set; }
+        public string Gender { get; set; }
+        public string CharacterPicture { get; set; }
+    }
+}
---
+++ Models/DTOs/CharacterProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+namespace MovieCharacterAPI.Models.DTOs
+{
+    public class CharacterProfile : Profile
+    {
+        public CharacterProfile()
+        {
+            CreateMap<Character, CharacterDto>();
+            CreateMap<CharacterCreateDto, Character>();
+            CreateMap<CharacterCreateDto, CharacterDto>();
+        }
+    }
+}
