1cb86a4979d2be43c0e605aea87ad629fb9bf725
arttuhar <arttuhar@gmail.com>
2022-03-03 12:34:49 +0200

    FEAT: Use repository in franchise methods

--- Models/DTOs/FranchiseDto.cs
+++ Models/DTOs/FranchiseDto.cs
@@ -5,8 +5,9 @@ namespace MovieCharacterAPI.Models.DTOs
 {
     public class FranchiseDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public ICollection<MovieDto> Movies { get; set; }
+        public List<int> Movies { get; set; }
     }
 }
--- Repositories/FranchiseRepository.cs
+++ Repositories/FranchiseRepository.cs
@@ -23,14 +23,22 @@ namespace MovieCharacterAPI.Repositories
         }
 
         /// <summary>
+        /// Check if franchise exists in database
+        /// </summary>
+        /// <param name="id">Id of franchise</param>
+        /// <returns></returns>
+        public bool FranchiseExists(int id)
+        {
+            return _context.Franchises.Any(f => f.Id == id);
+        }
+
+        /// <summary>
         /// Get all franchises
         /// </summary>
         /// <returns>All franchises</returns>
-        [HttpGet]
-        public async Task<IEnumerable<FranchiseDto>> GetAllFranchises()
+        public async Task<IEnumerable<Franchise>> GetAllFranchisesAsync()
         {
-            var list = await _context.Franchises.ToListAsync();
-            return _mapper.Map<IEnumerable<FranchiseDto>>(list);
+            return await _context.Franchises.Include(f => f.Movies).ToListAsync();
         }
 
         /// <summary>
@@ -38,67 +46,32 @@ namespace MovieCharacterAPI.Repositories
         /// </summary>
         /// <param name="id">Id of franchise</param>
         /// <returns>Franchise by given id</returns>
-        [HttpGet("{id}")]
-        public async Task<FranchiseDto> GetFranchise(int id)
+        public async Task<Franchise> GetFranchiseAsync(int id)
         {
-            var franchise = await _context.Franchises.FindAsync(id);
-            if (franchise == null)
-            {
-                return null;
-            }
-
-            var franchiseRead = _mapper.Map<FranchiseDto>(franchise);
-            return franchiseRead;
+            return await _context.Franchises.FindAsync(id);
         }
 
         /// <summary>
         /// Update selected franchise
         /// </summary>
-        /// <param name="id">Id of franchise</param>
-        /// <param name="franchiseUpdate">Franchise</param>
+        /// <param name="franchise">Franchise</param>
         /// <returns></returns>
-        [HttpPut("{id}")]
-        public async Task<bool> PutFranchise(int id, FranchiseUpdateDto franchiseUpdate)
+        public async Task PutFranchiseAsync(Franchise franchise)
         {
-            if (id != franchiseUpdate.Id)
-            {
-                return false;
-            }
-
-            Franchise domainFranchise = _mapper.Map<Franchise>(franchiseUpdate);
-            _context.Entry(franchiseUpdate).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-                return true;
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!FranchiseExists(id))
-                {
-                    return false;
-                }
-                else
-                {
-                    throw;
-                }
-            }
+           _context.Entry(franchise).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
         }
 
         /// <summary>
         /// Add new franchise to database
         /// </summary>
-        /// <param name="franchiseCreate">Franchise</param>
+        /// <param name="franchise">Franchise</param>
         /// <returns></returns>
-        [HttpPost]
-        public async Task<FranchiseDto> PostFranchise(FranchiseCreateDto franchiseCreate)
+        public async Task<Franchise> PostFranchiseAsync(Franchise franchise)
         {
-            Franchise domainFranchise = _mapper.Map<Franchise>(franchiseCreate);
-            _context.Franchises.Add(domainFranchise);
+            _context.Franchises.Add(franchise);
             await _context.SaveChangesAsync();
-
-            return _mapper.Map<FranchiseDto>(domainFranchise);
+            return franchise;
         }
 
         /// <summary>
@@ -106,69 +79,39 @@ namespace MovieCharacterAPI.Repositories
         /// </summary>
         /// <param name="id">Id of franchise</param>
         /// <returns></returns>
-        [HttpDelete("{id}")]
-        public async Task<bool> DeleteFranchise(int id)
+        public async Task DeleteFranchiseAsync(int id)
         {
             var franchise = await _context.Franchises.FindAsync(id);
-            if (franchise == null)
-            {
-                return false;
-            }
-
             _context.Franchises.Remove(franchise);
             await _context.SaveChangesAsync();
-            return true;
         }
 
         /// <summary>
-        /// Update movies in franchise
+        /// Update movies in selected franchise
         /// </summary>
-        /// <param name="id">Id of franchise</param>
+        /// <param name="franchiseId">Id of franchise</param>
         /// <param name="movies">List of movies</param>
         /// <returns></returns>
-        [HttpPut("{id}/movies")]
-        public async Task<bool> UpdateFranchiseMovies(int id, [FromBody] List<int> movies)
+        /// <exception cref="KeyNotFoundException"></exception>
+        public async Task UpdateFranchiseMoviesAsync(int franchiseId, List<int> movies)
         {
-            if (!FranchiseExists(id))
-            {
-                return false;
-            }
-
-            Franchise franchiseMovies = await _context.Franchises.Include(f => f.Movies).Where(f => f.Id == id).FirstAsync();
-
+            Franchise updatedMovies = await _context.Franchises.Include(f => f.Movies).Where(f => f.Id == franchiseId).FirstAsync();
             List<Movie> newMovies = new();
+
             foreach (int movieId in movies)
             {
                 Movie newMovie = await _context.Movies.FindAsync(movieId);
+
                 if (newMovie == null)
                 {
-                    return false;
+                    throw new KeyNotFoundException();
                 }
 
                 newMovies.Add(newMovie);
             }
 
-            franchiseMovies.Movies = newMovies;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-                return true;
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// Check if franchise exists in database
-        /// </summary>
-        /// <param name="id">Id of franchise</param>
-        /// <returns></returns>
-        bool FranchiseExists(int id)
-        {
-            return _context.Franchises.Any(f => f.Id == id);
+            updatedMovies.Movies = newMovies;
+            await _context.SaveChangesAsync();
         }
     }
 }
